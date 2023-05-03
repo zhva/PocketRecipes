e5769924e0dc834fd9485d9a8a86c7ee583c7c3f
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-03 17:02:23 +0200

    adds WelcomeHeadline component

---
+++ src/components/WelcomeHeadline.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WelcomeHeadline.scss'
+
+export const WelcomeHeadline = () => {
+  return (
+        <h1>
+            <span>Welcome to</span>
+            <br></br>
+            PocketRecipes
+        </h1>
+  )
+}
