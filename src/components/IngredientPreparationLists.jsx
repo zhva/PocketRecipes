fbcd5155adfa23408284b3557d57970228256f26
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-04 14:08:33 +0200

    displays passed data

--- src/components/IngredientPreparationLists.jsx
+++ src/components/IngredientPreparationLists.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 
-export const IngredientPreparationLists = (props) => {
-  // comment out const and use props once we have a list of recipes
-  const ingredients = ['Pasta of your choice 250g', 'Two Tomatoes ', 'Chili flakes', 'Italian herbs', 'Parmigiano 25g']
-  const preparationSteps = ['Salt and bring water to a boil', 'Preheat pan and cut tomatoes and onions', 'Fry tomatoes and onion', 'Put pasta into boiling water', 'When the pasta is al dente drain them and and throw them into the pan with some chili flakes. Toss the pan a couple of times.']
-
+export const IngredientPreparationLists = ({ingredients, preparationSteps}) => {
   return (
         <div className='ingredients-preparation-list'>
             <h2>Ingredients</h2>
             <ol>
                 {ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
+                    <li key={ingredient.id}><span>{(index + 1)}.</span> {ingredient.name}</li>
                 ))}
             </ol>
             <h2>Preparation</h2>
             <ol>
                 {preparationSteps.map((prepStep, index) => (
-                    <li key={index}>{prepStep}</li>
+                    <li key={prepStep.id}><span>{(index + 1)}.</span> {prepStep.name}</li>
                 ))}
             </ol>
         </div>
