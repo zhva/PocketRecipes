bbf7838a0c80489171ab1861cb7cb03b6c66515c
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-04 13:15:46 +0200

    myRecipes component

---
+++ src/components/MyRecipes.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../scss/HomeFeed.scss'
+import { useObjectVal } from 'react-firebase-hooks/database'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth, database } from '../firebase'
+import { ref } from 'firebase/database'
+import { RecipeCard } from './RecipeCard'
+import { useNavigate } from 'react-router-dom'
+
+export const MyRecipes = () => {
+  const [user] = useAuthState(auth)
+  // Get a reference to the document
+  const recipeRef = ref(database, `users/${user?.uid}/recipes`)
+  const [recipes, loading] = useObjectVal(recipeRef)
+  const navigate = useNavigate()
+
+  const recipeId = Object.keys(recipes ?? {})[0]
+  console.log(recipeId)
+
+  return (
+    <div className='home-container'>
+      <div className='home-heading-container'>
+        <h1>PocketRecipes</h1>
+        <span>MyRecipes</span>
+      </div>
+      <div className='recipes-container'>
+      <h2>Current recipes</h2>
+      <div className='recipes-list-container' >
+        {loading && <p>Loading recipes...</p>}
+        {!loading && recipes && Object.entries(recipes ?? {}).map(([key, recipe]) => {
+            console.log({key})
+          return (
+            <div key={key}>
+              <RecipeCard
+                imageSrc={recipe.imageLink && recipe.imageLink}
+                title={recipe.name}
+                author=''
+                description={recipe.description}
+                onClick = {() => navigate(`/my-recipes/${recipeId}`)} //  diese id wird später definiert
+              />
+            </div>
+          )
+        })}
+        </div>
+      </div>
+    </div>
+  )
+}
