7c0bd1147ac13ff9464b747e91e9255f9b654d49
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-04 13:36:46 +0200

    new component for buttons for recipe functionality + added icons

---
+++ src/components/RecipeButtons.jsx
@@ -0,0 +1,18 @@
+import { React } from 'react'
+
+const RecipeButtons () => {
+    return(
+        <div className="recipe-buttons-container">
+            <button>
+                <img></img>
+            </button>
+            <button>
+                <img></img>
+            </button>
+            <button>
+                <img></img>
+            </button>
+
+        </div>
+    )
+}
