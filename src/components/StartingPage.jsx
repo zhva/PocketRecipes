611e06fa2a2a14e57cf13e631e234bf5a61d5677
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-05 10:42:10 +0200

    changed startingpage text

--- src/components/StartingPage.jsx
+++ src/components/StartingPage.jsx
@@ -17,16 +17,22 @@ export const StartingPage = () => {
             <WelcomeHeadline />
             <SubHeadline>Store all your favourite recipes in one place.</SubHeadline>
             <InfoText>
-                Worem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nunc vulputate libero et velit interdum, ac aliquet odio
-                mattis interdum. Worem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br></br>
-                <br></br>
-                Nunc vulputate libero et velit interdum, ac aliquet odio mattis interdum.
-                Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
-                libero et velit interdum, ac aliquet odio mattis interdum. Worem ipsum
-                dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et
-                velit interdum, ac aliquet odio mattis interdum.
+            Say hello to PocketRecipes, your new tool 
+            for all things cooking. Create, edit, and 
+            delete recipes with ease, while also checking 
+            out what&apos;s hot in the food world with our  
+            recipe feed. 
+            <br></br>
+            <br></br>
+            Don&apos;t forget to sign up for an 
+            account to keep track of your personal recipes. 
+            Whether you&apos;re a newbie in the kitchen or a seasoned 
+            pro, PocketRecipes has got your back. And guess 
+            what? It&apos;s all made to be used on your smartphone, 
+            so you can whip up a storm from wherever you are. 
+            <br></br>
+            <br></br>
+            Let&apos;s get cooking!
             </InfoText>
             <div className='buttons-container'>
               <Button type='button' onClick={() => navigate('/feed')}>Get Started</Button>
