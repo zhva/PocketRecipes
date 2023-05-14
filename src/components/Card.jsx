56f5ca85e5310adb3f35586d6faa18500d5474de
Thomas Mayrhofer <thomas@mayrhofer.at>
2023-05-14 22:16:38 +0200

    remove files for easier review

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Card = (props) => {
-  return (
-        <div className='content-card'>
-            {props.children}
-        </div>
-  )
-}
--- src/components/SubHeadline.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SubHeadline = (props) => {
-  return (
-        <h2>{props.children}</h2>
-  )
-}
