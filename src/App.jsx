52ce069e3bee14ee1f53c3d25b1af6af5e4f117f
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-27 18:53:25 +0200

    changes Routes into ProtectedRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -9,64 +9,50 @@ import { CreateRecipe } from './components/application/CreateRecipe'
 import { Recipe } from './components/application/Recipe'
 import { HomeFeed } from './components/application/HomeFeed'
 import { MyRecipes } from './components/application/MyRecipes'
-import { Popup } from './components/generic/Popup'
+import { ProtectedRoute } from './components/generic/ProtectedRoute'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from './firebase'
 
 
-function App () {
+function App() {
   const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<StartingPage/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/feed" element={<HomeFeed/>} />
-        {user && (
-          <>
-            <Route path="/create-recipe" element={<CreateRecipe/>} />
-            <Route path="/my-recipes/:recipeId" element={<Recipe></Recipe>} />
-            <Route path="/my-recipes" element={<MyRecipes></MyRecipes>} />
-          </>
-        )}
-        {!user && (
-          <>
-            <Route path="/create-recipe" element={
-              <Popup
-                title={'Create an account'}
-                linkText={'Back to the Login page'}
-                linkText2={'Don\'t have an account yet?'}>
-              {'In order to use this functionality please Log in or create an account if you haven\'t done so to get full access to all features.'}
-            </Popup>
-            } />
-            <Route path="/my-recipes/:recipeId" element={
-              <Popup
-                title={'Create an account'}
-                linkText={'Back to the Login page'}
-                linkText2={'Don\'t have an account yet?'}>
-              {'In order to use this functionality please Log in or create an account if you haven\'t done so to get full access to all features.'}
-            </Popup>
-            } />
-            <Route path="/my-recipes" element={
-              <Popup
-                title={'Create an account'}
-                linkText={'Back to the Login page'}
-                linkText2={'Don\'t have an account yet?'}>
-              {'In order to use this functionality please Log in or create an account if you haven\'t done so to get full access to all features.'}
-            </Popup>
-            } />
-          </>
-        )}
-        <Route path="/edit/:recipeId" element={<CreateRecipe/>} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<StartingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/feed" element={<HomeFeed />} />
+      <Route path="/create-recipe" element={
+        <ProtectedRoute isAllowed={user}>
+          <CreateRecipe />
+        </ProtectedRoute>
+        }/>
+      <Route path="/my-recipes/:recipeId" element={
+        <ProtectedRoute isAllowed={user}>
+          <Recipe />
+        </ProtectedRoute>
+        }/>
+      <Route path="/my-recipes" element={
+        <ProtectedRoute isAllowed={user}>
+          <MyRecipes />
+        </ProtectedRoute>
+      }/>
+      <Route path="/edit/:recipeId" element={
+        <ProtectedRoute isAllowed={user}>
+          <CreateRecipe />
+        </ProtectedRoute>
+      }/>
+    </Routes>
       <NavigationBar
-        startingPageRoute = {'/'}
-        myRecipesRoute = {'/my-recipes'}
-        feedRoute = {'/feed'}
-        newRecipeRoute = {'/create-recipe'}/>
+        startingPageRoute={'/'}
+        myRecipesRoute={'/my-recipes'}
+        feedRoute={'/feed'}
+        newRecipeRoute={'/create-recipe'}
+      />
     </div>
   )
 }
 
-export default App
+export default  App
--- src/components/generic/Popup.jsx
+++ src/components/generic/Popup.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import arrow from '../../icons/Arrow.svg'
+import { useNavigate } from 'react-router-dom';
 
 export const Popup = ({ title, children, linkText, linkText2}) => {
+  const navigate = useNavigate()
   return (
     <div className='popup-container'>
       <div className='popup-inner-container'>
           <h1>{title}</h1>
           <p>{children}</p>
           <div className='backlink-container'>
-            <button>
+            <button onClick= {() => navigate(`/login`)}>
               <img src={arrow} alt="back arow"/>
-              {<a href="/login">{linkText}</a>}
+              {<span>{linkText}</span>}
             </button>
           </div>
           <div className='backlink-container'>
-            <button>
+            <button onClick= {() => navigate(`/signup`)}>
               <img src={arrow} alt="back arow"/>
-              { <a href="/signup">{linkText2}</a> }
+              {<span>{linkText2}</span> }
             </button>
           </div>
       </div>
