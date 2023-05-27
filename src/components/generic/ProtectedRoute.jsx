a5b2a5ed4cb23ed7302ce1c60fa5b82b4416259d
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-27 18:52:40 +0200

    adds ProtectedRoute component

---
+++ src/components/generic/ProtectedRoute.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Popup } from './Popup'
+
+export const ProtectedRoute = ({ isAllowed, children }) => {
+    const [isOpen] = React.useState(!isAllowed)
+
+    if (isOpen) {
+      return (
+        <Popup
+          title={'Create an account'}
+          linkText={'Back to the Login page'}
+          linkText2={"Don't have an account yet?"}
+        >
+          {
+            "In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features."
+          }
+        </Popup>
+      )
+    }
+
+    return children
+  }
+
+
+
