f03de84dee749e1817cf2d79f152c77432b0c777
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-28 21:32:32 +0200

    users can save recipe from the feed to my recipes

--- src/components/generic/RecipeButtons.jsx
+++ src/components/generic/RecipeButtons.jsx
@@ -2,19 +2,23 @@ import { React, useState } from 'react'
 import deleteIcon from '../../icons/deleteIcon.svg'
 import shareIcon from '../../icons/shareIcon.svg'
 import editIcon from '../../icons/editIcon.svg'
+import saveIcon from '../../icons/saveIcon.svg'
 import { useNavigate } from 'react-router-dom'
-import { ref, remove } from 'firebase/database'
+import { ref, remove, push } from 'firebase/database'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, database } from '../../firebase'
 import { DeleteConfirmationPopup } from './DeleteConfirmationPopup'
+import { useObjectVal } from 'react-firebase-hooks/database'
 
 export const RecipeButtons = ({recipeId, path}) => {
     const navigate = useNavigate()
     const [user] = useAuthState(auth)
     const [showPopup, setShowPopup] = useState(false)
+    const recipeFeedRef = ref(database, `feed/recipes/${recipeId}`)
+    const [recipe, loading] = useObjectVal(recipeFeedRef)
 
     const deleteRecipeFromDB = () => {
-        const recipeRef = ref(database, `users/${user?.uid}/recipes/${recipeId.recipeId}`)
+        const recipeRef = ref(database, `users/${user?.uid}/recipes/${recipeId}`)
         remove(recipeRef)
             .then(() => {
                 navigate('/my-recipes')
@@ -23,12 +27,35 @@ export const RecipeButtons = ({recipeId, path}) => {
                 console.error('Error deleting recipe:', error)
             })
     }
+
     const handleDelete = () => {
         setShowPopup(true)
     }
 
     const handleClosePopup = () => {
         setShowPopup(false)
+    }
+
+    const handleSave = async () => {
+        const recipesRef = user?.uid ? ref(database, `users/${user.uid}/recipes`) : null
+
+        if(!loading && recipe) {
+            const recipeData = {
+                imageLink: recipe.imageLink,
+                timestamp: new Date().getTime(),
+                values: {
+                    ...recipe.values
+                }
+            }
+            console.log({recipeData})
+
+            try {
+                await push(recipesRef, recipeData)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
     }
 
     return(
@@ -41,16 +68,21 @@ export const RecipeButtons = ({recipeId, path}) => {
                         Do you really want to delete this recipe?
                 </DeleteConfirmationPopup>}
             {path === 'my-recipes' && (
-                <button className='recipe-delete' onClick={handleDelete}>
+                <button type="button" className='recipe-delete' onClick={handleDelete}>
                     <img src={deleteIcon} alt="Delete"></img>
                 </button>
             )}
-            <button>
-                <img src={shareIcon} onClick={() => {}} alt="Share"></img>
+            {path === 'feed' && (
+                <button type="button" className='recipe-save' onClick={handleSave}>
+                    <img src={saveIcon} alt="Save to my recipes"></img>
+                </button>
+            )}
+            <button type="button" onClick={() => {}}>
+                <img src={shareIcon} alt="Share"></img>
             </button>
             {path === 'my-recipes' && (
-                <button onClick={() => {navigate(`/edit/${recipeId.recipeId}`)}}>
-                    <img className="edit-button" src={editIcon} alt="Edit"></img>
+                <button type="button" className="edit-button" onClick={() => {navigate(`/edit/${recipeId.recipeId}`)}}>
+                    <img src={editIcon} alt="Edit"></img>
                 </button>
             )}
         </div>
--- src/components/generic/RecipeDescription.jsx
+++ src/components/generic/RecipeDescription.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-export const RecipeDescription = ({ description, descriptionErrors, handleChange }) => {
+export const RecipeDescription = ({ description, descriptionErrors, handleChange, mode='edit' }) => {
   return (
-    <div className='recipe-description'>
-        <h1>Description</h1>
+    <div className={`recipe-description ${mode === 'display' ? 'display-description': ''}`}>
+        <h2>Description</h2>
         <label htmlFor="descriptionId"></label>
-        <textarea
-            name='description'
-            id='descriptionId'
-            onChange={handleChange}
-            defaultValue={description}
-            maxLength={1001}>
-        </textarea>
+        {mode === 'display' && (
+          <p>description</p>
+        )}
+        {mode === 'edit' && (
+          <textarea
+              name='description'
+              id='descriptionId'
+              onChange={handleChange}
+              defaultValue={description}
+              maxLength={1001}>
+          </textarea>
+        )}
         { descriptionErrors && <div className='formik-errors'>{ descriptionErrors }</div> }
     </div>
   )
