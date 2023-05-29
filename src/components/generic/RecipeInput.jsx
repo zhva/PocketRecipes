25c2fd873700d7c6b38e22c1a63fab9b2e2f6fa9
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-29 10:57:29 +0200

    removed input shadows + changed card color

--- src/components/generic/RecipeInput.jsx
+++ src/components/generic/RecipeInput.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 export const RecipeInput = ({ value, uuid, handleDelete, name, handleChange, inputError }) => {
   return (
-    <div className="recipe-input-container" >
-      <label htmlFor={uuid}>
-        <textarea
-          onChange={handleChange}
-          type='text' name={name}
-          id={uuid}
-          defaultValue={value}
-          maxLength={401}>
-        </textarea>
-        <button type='button' onClick={handleDelete} className='delete-button'>X</button>
-      </label>
-      {inputError && <div className='formik-errors'>{inputError}</div>}
+    <div className='input-button-pair'>
+      <div className="recipe-input-container" >
+        <label htmlFor={uuid}>
+          <textarea
+            onChange={handleChange}
+            type='text' name={name}
+            id={uuid}
+            defaultValue={value}
+            maxLength={401}>
+          </textarea>
+        </label>
+        {inputError && <div className='formik-errors'>{inputError}</div>}
+      </div>
+      <button type='button' onClick={handleDelete} className='delete-button'>X</button>
     </div>
   )
 }
