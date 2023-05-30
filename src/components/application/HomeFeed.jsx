f4d0f2fbbb91556cd6b9793fbd59d930be40cc50
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-30 14:10:33 +0200

    basic styling homefeed + see more option

--- src/components/application/HomeFeed.jsx
+++ src/components/application/HomeFeed.jsx
@@ -1,58 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useObjectVal } from 'react-firebase-hooks/database'
 import { database } from '../../firebase'
 import { ref } from 'firebase/database'
-import { RecipeCard } from '../generic/RecipeCard'
-import { useNavigate } from 'react-router-dom'
+import { Button } from '../generic/Button'
+// import { RecipeCard } from '../generic/RecipeCard'
+// import { useNavigate } from 'react-router-dom'
 
 export const HomeFeed = () => {
   // Get a reference to the document
+  const [seeMore, setSeeMore] = useState(false)
   const recipeRef = ref(database, `feed/recipes`)
   const [recipes, loading] = useObjectVal(recipeRef)
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
+
+  const toggleSeeMore = () => {
+    setSeeMore(!seeMore);
+    }
 
   return (
     <div className='home-container'>
       <div className='recipes-container'>
-{/* <<<<<<< HEAD
-        <h1>Homefeed</h1>
-        <p>
-          Discover Culinary Creations: A community feed of shared recipes at your fingertips!
-          <br></br>
-          <br></br>
-          Check them out now!
-        </p>
-        
-        <div className='recipes-list-container' >
-          {loading && <p>Loading recipes...</p>}
-          {!loading && recipes && Object.entries(recipes ?? {}).slice(0, 1).map(([key, recipe]) => {
-            return (
-              <div
-                className='first-recipe'
-                key={key}
-                onClick = {() => navigate(`/feed/${key}`)}>
-                <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink} />
-                <h3>{recipe.values.name}</h3>
-                <span>{recipe.author}</span>
-                <p>{recipe.values.description}</p>
-              </div>
-            )
-          })}
-          {loading && <p>Loading recipes...</p>}
-          {!loading && recipes && Object.entries(recipes ?? {}).slice(1).map(([key, recipe]) => {
-            return (
-              <div key={key}>
-                <RecipeCard
-                  imageSrc={recipe.imageLink && recipe.imageLink}
-                  title={recipe.values.name}
-                  author=''
-                  description={recipe.values.description}
-                  onClick = {() => navigate(`/feed/${key}`)}
-                />
-              </div>
-            )
-          })}
-======= */}
       <h1>Homefeed</h1>
         <p>
           Discover Culinary Creations: A community feed of shared recipes at your fingertips!
@@ -60,8 +27,40 @@ export const HomeFeed = () => {
           <br></br>
           Check them out now!
         </p>
-      <div className='recipes-list-container' >
+      <div className='recipe-feed'>
         {loading && <p>Loading recipes...</p>}
+        {!loading && recipes && Object.entries(recipes ?? {}).slice(0, 1).map(([key, recipe]) => {
+          return (
+            <div key = {key} className='main-recipe'>
+                <h2>{recipe.values.name}</h2>
+                <div className='main-recipe-img'>
+                  <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                </div>
+                <p>{recipe.values.description}</p>
+            </div>)})}
+            <div className='other-recipes'>
+            {loading && <p>Loading recipes...</p>}
+            {!seeMore && !loading && recipes && Object.entries(recipes ?? {}).slice(1, 5).map(([key, recipe]) => {
+              return (
+                  <div key={key} className='small-recipe'>
+                      <div className='img-container'>
+                        <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                      </div>
+                      <h2>{recipe.values.name}</h2>
+                  </div>)})}
+            {seeMore && !loading && recipes && Object.entries(recipes ?? {}).slice(1).map(([key, recipe]) => {
+              return (
+                  <div key={key} className='small-recipe'>
+                      <div className='img-container'>
+                        <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                      </div>
+                      <h2>{recipe.values.name}</h2>
+                  </div>)})}
+            </div>
+            <Button onClick={toggleSeeMore}>See more..</Button>
+      </div>
+      <div className='recipes-list-container' >
+        {/* {loading && <p>Loading recipes...</p>}
         {!loading && recipes && Object.entries(recipes ?? {}).slice(0, 1).map(([key, recipe]) => {
           return (
             <div
@@ -74,8 +73,8 @@ export const HomeFeed = () => {
               <p>{recipe.values.description}</p>
             </div>
           )
-        })}
-        {loading && <p>Loading recipes...</p>}
+        })} */}
+        {/* {loading && <p>Loading recipes...</p>}
         {!loading && recipes && Object.entries(recipes ?? {}).slice(1).map(([key, recipe]) => {
           return (
             <div key={key}>
@@ -88,7 +87,7 @@ export const HomeFeed = () => {
               />
             </div>
           )
-        })}
+        })} */}
         </div>
       </div>
     </div>
