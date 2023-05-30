988208a3b3120da386eb69f8c31c0c8cb7aacf3a
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-30 16:03:32 +0200

    simple new layout for myrecipes

--- src/components/application/MyRecipes.jsx
+++ src/components/application/MyRecipes.jsx
@@ -26,18 +26,44 @@ export const MyRecipes = () => {
           {`This is your "MyRecipes" section - a culinary 
           sanctuary where your creativity comes to life. 
           Here, you'll find all the recipes you've meticulously 
-          crafted and lovingly saved. Each recipe represents 
-          a piece of your unique journey through the world 
-          of cooking.`}
+          crafted.`}
           <br></br>
           <br></br>
-          {`Every dish you've added, from family favorites to 
-          experimental dishes, is stored right here for easy 
-          access. You can revisit them anytime you want to 
+          {`Every dish you've created, from family favorites to 
+          experimental dishes, is stored right here for easy access.
+          You can revisit them anytime you want to 
           recreate a meal, share your delicious creations with 
           others, or refine your recipes with new inspirations.`}
         </p>
         <div className='recipes-container'>
+        <div className='recipe-list-container'>
+          <div className='left-column'>
+            {!loading && recipes && Object.entries(recipes ?? {})
+              .filter((_, index) => index % 2 === 0)
+              .map(([key, recipe]) => (
+                <div key = {key} className='left-recipe'>
+                  <div className='recipe-img-container'>
+                    <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                  </div>
+                  <h2>{recipe.values.name}</h2>
+                  <p>{recipe.values.description}</p>
+                </div>
+            ))}
+          </div>
+          <div className='right-column'>
+            {!loading && recipes && Object.entries(recipes ?? {})
+              .filter((_, index) => index % 2 === 1)
+              .map(([key, recipe]) => (
+                <div key = {key} className='right-recipe'>
+                  <div className='recipe-img-container'>
+                    <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                  </div>
+                  <h2>{recipe.values.name}</h2>
+                  <p>{recipe.values.description}</p>
+                </div>
+            ))}
+          </div>
+          </div>
         <div className='recipes-list-container' >
           {!loading && recipes && Object.entries(recipes ?? {}).map(([key, recipe]) => {
             return (
