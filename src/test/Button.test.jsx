f5651b28cbee014c3fccccc4555404106a0845b5
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-30 15:06:10 +0200

    adds unit tests for Button component

---
+++ src/test/Button.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { Button } from '../components/generic/Button'
+
+describe('<Button />', () => {
+  let variant, onClick;
+
+  beforeEach(() => {
+    variant = 'default';
+    onClick = jest.fn();
+  })
+
+  test('renders correctly', () => {
+    const { getByRole } = render(
+      <Button variant={variant} onClick={onClick}>Button Text</Button>
+    );
+    expect(getByRole('button')).toHaveTextContent('Button Text');
+  });
+
+  test('responds to click events', () => {
+    const { getByRole } = render(
+      <Button variant={variant} onClick={onClick}>Button Text</Button>
+    );
+    fireEvent.click(getByRole('button'));
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  test('has correct className with default variant', () => {
+    const { getByRole } = render(
+      <Button variant={variant} onClick={onClick}>Button Text</Button>
+    );
+    expect(getByRole('button')).toHaveClass('custom-button');
+  });
+
+  test('has correct className with secondary variant', () => {
+    variant = 'secondary';
+    const { getByRole } = render(
+      <Button variant={variant} onClick={onClick}>Button Text</Button>
+    );
+    expect(getByRole('button')).toHaveClass('custom-button secondary');
+  });
+
+  test('has correct className with non-secondary variant', () => {
+    variant = 'not-secondary'; // or simply 'default'
+    const { getByRole } = render(
+      <Button variant={variant} onClick={onClick}>Button Text</Button>
+    );
+    expect(getByRole('button')).toHaveClass('custom-button');
+  });
+});
+
