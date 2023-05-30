76286f63e43b8e7c5852ad68569e515e944887f0
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-05-30 21:29:07 +0200

    myrecipes page styling

--- src/components/application/MyRecipes.jsx
+++ src/components/application/MyRecipes.jsx
@@ -3,19 +3,19 @@ import { useObjectVal } from 'react-firebase-hooks/database'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, database } from '../../firebase'
 import { ref } from 'firebase/database'
-import { RecipeCard } from '../generic/RecipeCard'
+// import { RecipeCard } from '../generic/RecipeCard'
 import { useNavigate } from 'react-router-dom'
 
 export const MyRecipes = () => {
   const [user] = useAuthState(auth)
   // Get a reference to the document
   const recipeRef = ref(database, `users/${user?.uid}/recipes`)
-  const userNameRef = ref(database, `users/${user?.uid}/name`)
+  // const userNameRef = ref(database, `users/${user?.uid}/name`)
   const [recipes, loading] = useObjectVal(recipeRef)
-  const [userName, nameLoading ]= useObjectVal(userNameRef)
+  // const [userName, nameLoading ]= useObjectVal(userNameRef)
   const navigate = useNavigate()
 
-  if(!loading && !nameLoading) {
+  if(!loading) {
     return (
       <div className='home-container'>
         <h1>My Recipes</h1>
@@ -36,48 +36,37 @@ export const MyRecipes = () => {
           others, or refine your recipes with new inspirations.`}
         </p>
         <div className='recipes-container'>
-        <div className='recipe-list-container'>
-          <div className='left-column'>
-            {!loading && recipes && Object.entries(recipes ?? {})
-              .filter((_, index) => index % 2 === 0)
-              .map(([key, recipe]) => (
-                <div key = {key} className='left-recipe'>
-                  <div className='recipe-img-container'>
-                    <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+          <div className='recipe-list-container'>
+            <div className='column left-column'>
+              {!loading && recipes && Object.entries(recipes ?? {})
+                .filter((_, index) => index % 2 === 0)
+                .map(([key, recipe]) => (
+                  <div key={key} onClick = {() => navigate(`/my-recipes/${key}`)} className='recipe'>
+                    <div className='recipe-img-container'>
+                      <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                    </div>
+                    <div className='recipe-text'>
+                      <h2>{recipe.values.name}</h2>
+                      <p>{recipe.values.description}</p>
+                    </div>
                   </div>
-                  <h2>{recipe.values.name}</h2>
-                  <p>{recipe.values.description}</p>
-                </div>
-            ))}
-          </div>
-          <div className='right-column'>
-            {!loading && recipes && Object.entries(recipes ?? {})
-              .filter((_, index) => index % 2 === 1)
-              .map(([key, recipe]) => (
-                <div key = {key} className='right-recipe'>
-                  <div className='recipe-img-container'>
-                    <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+              ))}
+            </div>
+            <div className='column right-column'>
+              {!loading && recipes && Object.entries(recipes ?? {})
+                .filter((_, index) => index % 2 === 1)
+                .map(([key, recipe]) => (
+                  <div key={key} onClick = {() => navigate(`/my-recipes/${key}`)} className='recipe'>
+                    <div className='recipe-img-container'>
+                      <img src={recipe.imageLink && recipe.imageLink} alt={recipe.imageLink}></img>
+                    </div>
+                    <div className='recipe-text'>
+                      <h2>{recipe.values.name}</h2>
+                      <p>{recipe.values.description}</p>
+                    </div>
                   </div>
-                  <h2>{recipe.values.name}</h2>
-                  <p>{recipe.values.description}</p>
-                </div>
-            ))}
-          </div>
-          </div>
-        <div className='recipes-list-container' >
-          {!loading && recipes && Object.entries(recipes ?? {}).map(([key, recipe]) => {
-            return (
-              <div key={key}>
-                <RecipeCard
-                  imageSrc={recipe.imageLink && recipe.imageLink}
-                  title={recipe.values.name}
-                  author={userName}
-                  description={recipe.values.description}
-                  onClick = {() => navigate(`/my-recipes/${key}`)}
-                />
-              </div>
-            )
-          })}
+              ))}
+            </div>
           </div>
         </div>
       </div>
