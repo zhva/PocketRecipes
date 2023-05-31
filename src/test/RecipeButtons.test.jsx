805351e321cee96223e21e1df5f11780b4e6610b
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-05-31 13:28:23 +0200

    adds more unit tests for RecipeButtons component

--- src/test/RecipeButtons.test.jsx
+++ src/test/RecipeButtons.test.jsx
@@ -1,33 +1,93 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { MemoryRouter, Route, Routes } from 'react-router-dom'
-import { ProtectedRoute } from '../components/generic/ProtectedRoute'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { useNavigate } from 'react-router-dom'
+import { RecipeButtons } from '../components/generic/RecipeButtons'
+import { getDatabase, ref, remove } from 'firebase/database'
+import 'firebase/database'
 
-describe('<ProtectedRoute />', () => {
+// jest.mock('firebase/database', () => {
+//   const recipeRef = {}
+//   const mockRef = jest.fn().mockReturnValue(recipeRef)
+//   const mockRemove = jest.fn().mockResolvedValue()
+//   const mockGetDatabase = jest.fn(() => ({
+//     ref: mockRef,
+//     remove: mockRemove,
+//   }))
 
-  test('renders Popup when isAllowed is false', () => {
+//   return {
+//     getDatabase: mockGetDatabase,
+//     ref: mockRef,
+//     remove: mockRemove,
+//   }
+// })
+
+
+const mockUser = {
+  uid: '123456'
+}
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useContext: () => mockUser
+}))
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}))
+
+let mockNavigate
+
+beforeEach(() => {
+  mockNavigate = jest.fn()
+  useNavigate.mockImplementation(() => mockNavigate)
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('<RecipyButtons />', () => {
+
+  test('renders RecipeButtons component without crashing', () => {
     render(
-      <MemoryRouter initialEntries={['/protected']}>
-        <Routes>
-          <Route path="/protected" element={<ProtectedRoute isAllowed={false}>Protected Content</ProtectedRoute>} />
-        </Routes>
-      </MemoryRouter>
+      <RecipeButtons recipeId="1" path="feed" />
     )
-
-    expect(screen.getByText('Create an account')).toBeInTheDocument()
-    expect(screen.getByText("In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features.")).toBeInTheDocument()
   })
 
-  test('renders children when isAllowed is true', () => {
+  test('shows delete confirmation popup when delete button is clicked', () => {
     render(
-      <MemoryRouter initialEntries={['/protected']}>
-        <Routes>
-          <Route path="/protected" element={<ProtectedRoute isAllowed={true}>Protected Content</ProtectedRoute>} />
-        </Routes>
-      </MemoryRouter>
+      <RecipeButtons recipeId="1" path="my-recipes" />
     )
+    const deleteButton = screen.getByRole('button', { name: /delete/i })
+    fireEvent.click(deleteButton)
 
-    expect(screen.getByText('Protected Content')).toBeInTheDocument()
+    const deletePopup = screen.getByText(/do you really want to delete this recipe/i)
+    expect(deletePopup).toBeInTheDocument()
   })
 
+  // test('calls Firebase remove function when delete button is clicked', async () => {
+  //   render(
+  //     <RecipeButtons recipeId="1" path="my-recipes" />
+  //   )
+
+  //   const deleteButton = screen.getByRole('button', { name: /delete/i })
+  //   fireEvent.click(deleteButton)
+  //   const confirmButton = screen.getByRole('button', { name: /confirm/i })
+  //   fireEvent.click(confirmButton)
+
+  //   await waitFor(() => {
+  //     expect(remove).toHaveBeenCalledWith(recipeRef)
+  //   })
+  // })
+
+  test('calls navigate function with correct parameter when edit button is clicked', () => {
+    render(
+      <RecipeButtons recipeId="1" path="my-recipes" />
+    )
+    const editButton = screen.getByRole('button', { name: /edit/i })
+    fireEvent.click(editButton)
+
+    expect(mockNavigate).toHaveBeenCalledWith(`/edit/1`)
+  })
 })
