b8664450552cd4a03d4d3de9ba63398995507d87
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-01 18:01:31 +0200

    fixes height for ProtectedRoute

--- src/components/generic/ProtectedRoute.jsx
+++ src/components/generic/ProtectedRoute.jsx
@@ -6,18 +6,20 @@ export const ProtectedRoute = ({ isAllowed, children }) => {
 
     if (isOpen) {
       return (
-        <Popup
-          title={'Create an account'}
-          linkText={'Back to the Login page'}
-          redirectLink={'/login'}
-          linkText2={"Don't have an account yet?"}
-          redirectLink2={'/signup'}
-          mode = 'protected'
-        >
-          {
-            "In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features."
-          }
-        </Popup>
+        <div className='protected-route-container'>
+          <Popup
+            title={'Create an account'}
+            linkText={'Back to the Login page'}
+            redirectLink={'/login'}
+            linkText2={"Don't have an account yet?"}
+            redirectLink2={'/signup'}
+            mode = 'protected'
+          >
+            {
+              "In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features."
+            }
+          </Popup>
+        </div>
       )
     }
 
