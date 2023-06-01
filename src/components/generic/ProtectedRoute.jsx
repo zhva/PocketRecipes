77bb3fa449c385b208e9403da34670c3e3069897
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-01 18:11:01 +0200

    fixes width in protected-route-container

--- src/components/generic/ProtectedRoute.jsx
+++ src/components/generic/ProtectedRoute.jsx
@@ -6,20 +6,22 @@ export const ProtectedRoute = ({ isAllowed, children }) => {
 
     if (isOpen) {
       return (
-        <div className='protected-route-container'>
-          <Popup
-            title={'Create an account'}
-            linkText={'Back to the Login page'}
-            redirectLink={'/login'}
-            linkText2={"Don't have an account yet?"}
-            redirectLink2={'/signup'}
-            mode = 'protected'
-          >
-            {
-              "In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features."
-            }
-          </Popup>
-        </div>
+        <>
+          <div className='protected-route-container'>
+          </div>
+            <Popup
+              title={'Create an account'}
+              linkText={'Back to the Login page'}
+              redirectLink={'/login'}
+              linkText2={"Don't have an account yet?"}
+              redirectLink2={'/signup'}
+              mode = 'protected'
+            >
+              {
+                "In order to use this functionality please Log in or create an account if you haven't done so to get full access to all features."
+              }
+            </Popup>
+          </>
       )
     }
 
