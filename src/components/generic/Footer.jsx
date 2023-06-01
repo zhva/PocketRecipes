2ffc13eff068f76eb78a5f1eba83ad873a331d75
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-01 22:22:18 +0200

    deletes unused fonts

--- src/components/generic/Footer.jsx
+++ src/components/generic/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className='footer-container'>
            <div>
                 <small>
-                    <a href="https://www.freepik.com/free-vector/male-chef-mixing-flour-eggs-dough-with-whisk-hands-happy-man-apron-preparing-homemade-sweet-dessert-table-home-kitchen-flat-vector-illustration-pastry-cooking-recipe-concept_22343895.htm#query=cooking&position=4&from_view=search&track=sph#position=4&query=cooking">Starting page Image by pch.vector</a>
+                    <a href="https://www.freepik.com/free-vector/recipe-book-concept-illustration_19245712.htm#query=cooking%20recipe&position=19&from_view=keyword&track=ais">Starting page Image by freepik</a>
                 </small>
                 <small>
                     <a href="https://www.freepik.com/free-vector/female-chef-concept-illustration_32318741.htm#query=cooking&position=23&from_view=search&track=sph">Sign-in/Sign-up Image by storyset</a>
