372da9fff3d3f9e43418f9d139f7fa0d838f4f04
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-02 11:13:42 +0200

    adds auto resize to description field

--- src/components/generic/RecipeDescription.jsx
+++ src/components/generic/RecipeDescription.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export const RecipeDescription = ({ description, descriptionErrors, handleChange, mode='edit' }) => {
+  const textareaRef = useRef()
+
+  useEffect(() => {
+    const handleInput = () => {
+      textareaRef.current.style.height = 'auto'
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+    }
+    const textareaElement = textareaRef.current
+    textareaElement.addEventListener('input', handleInput)
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      textareaElement.removeEventListener('input', handleInput)
+    }
+  }, [])
   return (
     <div className={`recipe-description ${mode === 'display' ? 'display-description': ''}`}>
         <h2>Description</h2>
@@ -10,11 +25,12 @@ export const RecipeDescription = ({ description, descriptionErrors, handleChange
         )}
         {mode === 'edit' && (
           <textarea
-              name='description'
-              id='descriptionId'
-              onChange={handleChange}
-              defaultValue={description}
-              maxLength={1001}>
+            ref={textareaRef}
+            name='description'
+            id='descriptionId'
+            onChange={handleChange}
+            defaultValue={description}
+            maxLength={1001}>
           </textarea>
         )}
         { descriptionErrors && <div className='formik-errors'>{ descriptionErrors }</div> }
