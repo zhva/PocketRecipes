ad8e20cebca792e7a3e60e13ec4e4a87bae299f5
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-02 10:37:20 +0200

    replaces X into image in delete button

--- src/components/generic/RecipeInput.jsx
+++ src/components/generic/RecipeInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import deleteIcon from '../../icons/deleteIcon.svg'
 
 export const RecipeInput = ({ value, uuid, handleDelete, name, handleChange, inputError, index }) => {
   return (
@@ -16,7 +17,9 @@ export const RecipeInput = ({ value, uuid, handleDelete, name, handleChange, inp
         </label>
         {inputError && <div className='formik-errors'>{inputError}</div>}
       </div>
-      <button type='button' onClick={handleDelete} className='delete-button'>X</button>
+      <button type='button' onClick={handleDelete} className='delete-button'>
+      <img src={deleteIcon} alt='Delete'></img>
+      </button>
     </div>
   )
 }
