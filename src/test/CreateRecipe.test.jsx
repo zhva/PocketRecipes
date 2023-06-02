604e18160b6e6f6f95888eacb9c4e5c216e748c0
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-02 11:42:17 +0200

    fixes failed unit test

--- src/test/CreateRecipe.test.jsx
+++ src/test/CreateRecipe.test.jsx
@@ -1,47 +1,50 @@
-import { cleanup, render, screen, act } from '@testing-library/react'
+import { cleanup, render, act } from '@testing-library/react'
 import React from 'react'
 import { CreateRecipe } from '../components/application/CreateRecipe'
 
 jest.mock('firebase/database', () => ({
-    getDatabase: jest.fn(() => ({
-      ref: jest.fn(),
-    })),
+  getDatabase: jest.fn(() => ({
     ref: jest.fn(),
-    push: jest.fn(),
-    update: jest.fn(),
-    remove: jest.fn(),
-  }))
+  })),
+  ref: jest.fn(),
+  push: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+}))
 
 jest.mock('firebase/storage', () => ({
-    getStorage: jest.fn(() => ({
-      ref: jest.fn(),
-    })),
+  getStorage: jest.fn(() => ({
     ref: jest.fn(),
-    uploadBytes: jest.fn(),
-    getDownloadURL: jest.fn(),
-  }))
+  })),
+  ref: jest.fn(),
+  uploadBytes: jest.fn(),
+  getDownloadURL: jest.fn(),
+}))
 
 jest.mock('firebase/auth', () => ({
-    getAuth: jest.fn(() => ({})),
-  }))
+  getAuth: jest.fn(() => ({})),
+}))
 
 jest.mock('react-firebase-hooks/database', () => ({
-    useObjectVal: () => [null, false],
-  }))
+  useObjectVal: () => [null, false],
+}))
 
 jest.mock('react-firebase-hooks/auth', () => ({
-    useAuthState: () => [{}],
-  }))
+  useAuthState: () => [{}],
+}))
 
 jest.mock('react-router-dom', () => ({
-    useNavigate: () => jest.fn(),
-    useParams: () => ({}),
-  }))
+  useNavigate: () => jest.fn(),
+  useParams: () => ({}),
+}))
 
-afterEach(cleanup)
+afterEach(() => {
+  cleanup()
+  jest.resetAllMocks()
+})
 
 test('CreateRecipe renders without crashing', async () => {
-  await act(() => {
-    render(<CreateRecipe />);
+  await act(async () => {
+    render(<CreateRecipe />)
   })
 })
