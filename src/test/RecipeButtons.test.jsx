f8dc496b0ad41959bde7f0844418ae11f1e4997f
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-06-12 19:54:04 +0200

    fixes failed tests

--- src/test/RecipeButtons.test.jsx
+++ src/test/RecipeButtons.test.jsx
@@ -1,10 +1,11 @@
-import { render, fireEvent, waitFor, findByText } from '@testing-library/react';
-import { RecipeButtons } from '../components/generic/RecipeButtons';
-import { useObjectVal } from 'react-firebase-hooks/database';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import clipboardCopy from 'clipboard-copy';
+import { render, fireEvent, waitFor, findByText } from '@testing-library/react'
+import { RecipeButtons } from '../components/generic/RecipeButtons'
+import { useObjectVal } from 'react-firebase-hooks/database'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate } from 'react-router-dom'
+import clipboardCopy from 'clipboard-copy'
 
+window.alert = jest.fn()
 
 jest.mock('clipboard-copy', () => jest.fn());
 
@@ -27,7 +28,7 @@ describe('RecipeButtons', () => {
   const mockNavigate = useNavigate();
 
   beforeEach(() => {
-    useAuthState.mockImplementation(() => [{ uid: '1' }]);
+    useAuthState.mockImplementation(() => [{ uid: '1' }])
     useObjectVal.mockImplementation(() => [
       {
         imageLink: 'test-imageLink',
@@ -43,54 +44,54 @@ describe('RecipeButtons', () => {
   });
 
   afterEach(() => {
-    useAuthState.mockClear();
-    useObjectVal.mockClear();
-    clipboardCopy.mockClear();
-  });
+    useAuthState.mockClear()
+    useObjectVal.mockClear()
+    clipboardCopy.mockClear()
+  })
 
   it('renders without crashing', () => {
-    const { getByAltText } = render(<RecipeButtons recipeId="1" path="my-recipes" />);
-    expect(getByAltText('Delete')).toBeInTheDocument();
-    expect(getByAltText('Share')).toBeInTheDocument();
-    expect(getByAltText('Edit')).toBeInTheDocument();
+    const { getByAltText } = render(<RecipeButtons recipeId="1" path="my-recipes" />)
+    expect(getByAltText('Delete')).toBeInTheDocument()
+    expect(getByAltText('Share')).toBeInTheDocument()
+    expect(getByAltText('Edit')).toBeInTheDocument()
   });
 
   it('opens delete confirmation popup when delete button is clicked', () => {
-    const { getByAltText, getByText } = render(<RecipeButtons recipeId="1" path="my-recipes" />);
-    fireEvent.click(getByAltText('Delete'));
-    expect(getByText('Delete Recipe')).toBeInTheDocument();
-    expect(getByText('Do you really want to delete this recipe?')).toBeInTheDocument();
-  });
+    const { getByAltText, getByText } = render(<RecipeButtons recipeId="1" path="my-recipes" />)
+    fireEvent.click(getByAltText('Delete'))
+    expect(getByText('Delete Recipe')).toBeInTheDocument()
+    expect(getByText('Do you really want to delete this recipe?')).toBeInTheDocument()
+  })
 
   it('opens save popup when save button is clicked', async () => {
-    const { getByAltText, findByText } = render(<RecipeButtons recipeId="1" path="feed" />);
-    fireEvent.click(getByAltText('Save to my recipes'));
+    const { getByAltText, findByText } = render(<RecipeButtons recipeId="1" path="feed" />)
+    fireEvent.click(getByAltText('Save to my recipes'))
 
-    const recipeSavedText = await findByText('Recipe Saved');
-    expect(recipeSavedText).toBeInTheDocument();
+    const recipeSavedText = await findByText('Recipe Saved')
+    expect(recipeSavedText).toBeInTheDocument()
 
-    const savedToYourRecipesText = await findByText('The recipe has been saved to your recipes.');
-    expect(savedToYourRecipesText).toBeInTheDocument();
-  });
+    const savedToYourRecipesText = await findByText('The recipe has been saved to your recipes.')
+    expect(savedToYourRecipesText).toBeInTheDocument()
+  })
 
   it('opens share popup and copies link to clipboard when share button is clicked', async () => {
-    const { getByAltText, findByText } = render(<RecipeButtons recipeId="1" path="my-recipes" />);
-    fireEvent.click(getByAltText('Share'));
+    const { getByAltText, findByText } = render(<RecipeButtons recipeId="1" path="my-recipes" />)
+    fireEvent.click(getByAltText('Share'))
 
-    const recipeSharedText = await findByText('Share Recipe');
-    expect(recipeSharedText).toBeInTheDocument();
+    const recipeSharedText = await findByText('Share Recipe')
+    expect(recipeSharedText).toBeInTheDocument()
 
-    const sharedLinkCopiedText = await findByText('The link has been copied to your clipboard. You are now able to share the recipe link.');
-    expect(sharedLinkCopiedText).toBeInTheDocument();
+    const sharedLinkCopiedText = await findByText('The link has been copied to your clipboard. You are now able to share the recipe link.')
+    expect(sharedLinkCopiedText).toBeInTheDocument()
 
-    expect(clipboardCopy).toHaveBeenCalled();
-  });
+    expect(clipboardCopy).toHaveBeenCalled()
+  })
 
   it('navigates to edit recipe when edit button is clicked', async () => {
-    const { getByAltText } = render(<RecipeButtons recipeId="1" path="my-recipes" />);
-    fireEvent.click(getByAltText('Edit'));
+    const { getByAltText } = render(<RecipeButtons recipeId="1" path="my-recipes" />)
+    fireEvent.click(getByAltText('Edit'))
 
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/edit/1'));
-  });
+    await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/edit/1'))
+  })
 
-});
+})
